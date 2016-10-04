8837bd3f084c42bf8ee19eb6d636dac6fba9f4f7
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2016-10-04 12:52:56 +0200

    Comment out migration for add_pages_xml_text_index_again

--- db/migrate/20130324195452_add_pages_xml_text_index_again.rb
+++ db/migrate/20130324195452_add_pages_xml_text_index_again.rb
@@ -1,6 +1,6 @@
 class AddPagesXmlTextIndexAgain < ActiveRecord::Migration
   def up
-    add_index(:pages, :xml_text, :name => 'pages_xml_text_index', :length => 1000)
+    #add_index(:pages, :xml_text, :name => 'pages_xml_text_index', :length => 1000)
   end
 
   def down
